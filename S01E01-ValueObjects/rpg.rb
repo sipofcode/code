42264333d211f03aac59cfc3938c78a8fea99f47
Lucas Videla <videla.lucas@gmail.com>
2019-03-08 14:27:59 -0300

    2. Introducimos el Value Object

--- S01E01-ValueObjects/rpg.rb
+++ S01E01-ValueObjects/rpg.rb
@@ -1,4 +1,4 @@
-class Character
+class Position
   attr_reader :x, :y
 
   def initialize(x, y)
@@ -9,9 +9,27 @@ class Character
     @x += dx
     @y += dy
   end
+end
+
+class Character
+  def initialize(x, y)
+    @position = Position.new(x, y)
+  end
+
+  def x
+    @position.x
+  end
+
+  def y
+    @position.y
+  end
+
+  def move(dx, dy)
+    @position.move(dx, dy)
+  end
 
   def in_range_with?(other)
-    Math.sqrt((other.x - @x) ** 2 +
-              (other.y - @y) ** 2) < 5
+    Math.sqrt((other.x - @position.x) ** 2 +
+              (other.y - @position.y) ** 2) < 5
   end
 end
