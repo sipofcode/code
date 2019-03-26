3f1811a82d82287b88003b9d4c236a97af0ff49d
Lucas Videla <videla.lucas@gmail.com>
2019-03-26 12:48:42 -0300

    S01E02: 1. Escenario inicial

---
+++ S01E02-ParameterObjects/rpg.rb
@@ -0,0 +1,41 @@
+class Character
+  attr_reader :weapon
+
+  def initialize
+    @lvl = @str = @dex = 1
+  end
+
+  def equip(weapon)
+    if weapon.can_be_used_with?(@lvl, @str, @dex)
+      @weapon = weapon
+    end
+  end
+end
+
+class Weapon
+  def initialize(lvl, str, dex)
+    @min_lvl = lvl
+    @min_str = str
+    @min_dex = dex
+  end
+
+  def can_be_used_with?(lvl, str, dex)
+    lvl >= @min_lvl &&
+    str >= @min_str &&
+    dex >= @min_dex
+  end
+end
+
+class Potion
+  def initialize(lvl, str, dex)
+    @min_lvl = lvl
+    @max_str = str
+    @min_dex = dex
+  end
+
+  def can_be_used_with?(lvl, str, dex)
+    lvl >= @min_lvl &&
+    str < @max_str &&
+    dex >= @min_dex
+  end
+end
---
+++ S01E02-ParameterObjects/rpg_tests.rb
@@ -0,0 +1,52 @@
+require "minitest/autorun"
+require "minitest/color"
+require "./rpg"
+
+class WeaponTest < Minitest::Test
+  def test_can_equip
+    weapon = Weapon.new(10, 100, 100)
+    assert weapon.can_be_used_with?(10, 100, 100)
+  end
+
+  def test_cannot_equip_low_level
+    weapon = Weapon.new(10, 100, 100)
+    refute weapon.can_be_used_with?(9, 100, 100)
+  end
+
+  def test_cannot_equip_low_strength
+    weapon = Weapon.new(10, 100, 100)
+    refute weapon.can_be_used_with?(10, 99, 100)
+  end
+
+  def test_cannot_equip_low_dexterity
+    weapon = Weapon.new(10, 100, 100)
+    refute weapon.can_be_used_with?(10, 100, 99)
+  end
+end
+
+class CharacterTest < Minitest::Test
+  def test_weak_character_cannot_equip
+    weapon = Weapon.new(10, 100, 100)
+    character = Character.new
+    character.equip(weapon)
+
+    assert_nil character.weapon
+  end
+
+  class StrongCharacter < Character
+    def initialize
+      @lvl = 10
+      @str = 100
+      @dex = 100
+    end
+  end
+
+  def test_strong_character_can_equip
+    weapon = Weapon.new(10, 100, 100)
+    character = StrongCharacter.new
+    character.equip(weapon)
+
+    refute_nil character.weapon
+    assert_equal weapon, character.weapon
+  end
+end
